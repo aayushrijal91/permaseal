222b80504cb4ce3206a9446142f0a090220555f9
Aayush Rijal <aayush.rijal99@gmail.com>
2022-06-03 12:36:27 +1000

    Site on staging

--- app/src/header.php
+++ app/src/header.php
@@ -20,9 +20,60 @@
 			});
 		});
 	</script>
+
+	<!-- Google Tag Manager -->
+	<script>
+		(function(w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-NJ6B7BZ');
+	</script>
+	<!-- End Google Tag Manager -->
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-47679308-84"></script>
+
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', 'AW-10910713044');
+		gtag('config', 'UA-47679308-84');
+	</script>
+
+	<!-- Global site tag (gtag.js) - Google Ads -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-10910713044"></script>
+
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', 'AW-10910713044');
+	</script>
 </head>
 
 <body>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJ6B7BZ" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+
 	<header>
 		<div class="header-top-bar">
 			<div class="container-fluid">
--- app/thankyou.php
+++ app/thankyou.php
@@ -1,3 +1,10 @@
+<!-- Event snippet for Contact Form conversion page -->
+<script>
+    gtag('event', 'conversion', {
+        'send_to': 'AW-10910713044/pk7jCP-G48ADENSJ0dIo'
+    });
+</script>
+
 <div class="thankyou-wrapper text-white text-center py-3" style="background-color: green;">
     Thank you! Your message has been sent. We will contact you shortly.
 </div>
